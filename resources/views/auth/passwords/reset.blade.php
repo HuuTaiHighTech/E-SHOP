daaea81e987153bf8cd4fdce9c977e288772b6c1
Nguyen Huu Tai <taipro573@gmail.com>
2024-04-06 13:39:07 +0700

    update file dockerfile

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -17,7 +17,12 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email"
+                                class="form-control @error('email') is-invalid @enderror"
+                                name="email" value="{{ $email ?? old('email') }}"
+                                required autocomplete="email" autofocus
+                                readonly
+                                style="pointer-events: none; user-select: none; background-color: #f3f3f3;">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
